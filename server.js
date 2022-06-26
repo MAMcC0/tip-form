25fbdc8b0d336d04d93f21344d73c8ac71ab84a3
Meredith McCollum <86750930+MAMcC0@users.noreply.github.com>
2022-06-26 15:30:06 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+import clog from './middleware/clog.js';
+import {api} from './routes/index.js';
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+// Import custom middleware, "cLog"
+app.use(clog);
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
